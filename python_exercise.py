471ff5b18cbb048f5526c9ae5d1c8c97952688e0
pengzhefu <32311379+pengzhefu@users.noreply.github.com>
2019-03-10 01:33:59 -0500

    Update python_exercise.py

--- python_exercise.py
+++ python_exercise.py
@@ -66,6 +66,17 @@ def findPrime(x,factor): #只能用于数字比较小的时候
 #    
 #print(findPrime(13195,2))
 #
+            
+"""对于给定的n, 使factor = 2, 3, 4, 5, 6...,
+对于每个factor, 当factor能被n完全整除时, 就到下一个factor.
+可以预见, 所有被整除的factor都是质因数,
+当所有小的因数都被整除时, n将会变为1
+如n为20, factor为2时, 20 % 2 = 0, n = n / 2, n变为10,
+return factor为10,
+10 % 2 = 0, n = n / 2, n变为5, (整除时将某一个因数整除完)
+然后下一个factor3, 4, 5, n % 5 = 0,
+return factor = 5, n变为1, 跳出循环
+"""
 def findPrimelarge(x,factor): #算数字比较大的时候
     if numberisPrime(x):
         return x
