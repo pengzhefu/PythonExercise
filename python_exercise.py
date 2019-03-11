5f53945308149ec6a50ec27f8e1257235d9c0827
pengzhefu <32311379+pengzhefu@users.noreply.github.com>
2019-03-10 22:23:01 -0400

    Update python_exercise.py

--- python_exercise.py
+++ python_exercise.py
@@ -92,3 +92,81 @@ def findPrimelarge(x,factor): #算数字比较大的时候
         return result
 #
 #print(findPrimelarge(600851475143,2))
+'''
+#----------------------------------------#
+Question 1
+Level 1
+
+Question:
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+between 2000 and 3200 (both included).
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+'''
+def solution1(x,y):
+    ret = []
+    for number in range(x,y+1):
+        if ((number%7) == 0) and ((number % 5)!= 0):
+            ret.append(str(number))
+    return ret
+#a = solution1(2000,3200)
+#print(','.join(a))  ##join方法必须保持前后的变量的一致性，前面要了str类型的，就要后面也是str
+
+'''
+Question 2
+Level 1
+
+Question:
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+40320
+'''
+def solution2(x):
+    if x == 1:
+        return x
+    else:
+        return x*solution2(x-1)
+#print(solution2(8))
+
+'''
+Question 3
+Level 1
+
+Question:
+With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+'''
+def solution3(n):
+    ret = []
+    for i in range(1,n+1):
+        ret.append((i,i*i))
+    dic = dict(ret)
+    return dic
+#print(solution3(17))
+
+'''
+Question 4
+Level 1
+
+Question:
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a 
+tuple which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+'''
+#str1 = input("Please input your number:")
+def solution5():
+    str1 = input("Please input your number:")
+    list1 = str1.split(',')
+    print(list1)
+    print(tuple(list1))
+#solution5()
